0479223c424acd993b3db15670b7a7534ba6a244
unknown <kerituni123@gmail.com>
2019-10-11 20:51:27 +0700

    make it better

--- app.js
+++ app.js
@@ -31,6 +31,6 @@ app.get('/users/:userid', (req, res) => {
 });
 
 // start the server at URL: http://localhost:3000/
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Server started at http://localhost:3000/');
 });
